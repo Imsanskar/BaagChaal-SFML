83d33bb3a4f1319413d136332a71ea09dc5b69eb
@Imsanskar <sanskaramgain@gmail.com>
2020-02-14 17:51:10 +0545

    Check  game over method added

--- includes/Game.h
+++ includes/Game.h
@@ -7,19 +7,24 @@
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics.hpp>
 #include "Loadboard.h"
+
+
+
 class Game
 {
 private:
     sf::Event event;
-    Board board;
     Goat goat[20];
-    int goatChosen=0;
-    bool gameOver=false,tigerTurn=true,goatTurn;
+    Board board;
+    int goatChosen,goatEaten;
+    bool gameOver=false,tigerTurn=true;
+    bool tigerWin,goatWin;
 public:
     Game(unsigned int ,unsigned int );
     void run();
     void processEvents();
     void handlePlayerInput(sf::Keyboard::Key & );
+    void checkGameOver();
 protected:
     sf::RenderWindow mWindow;
 };
